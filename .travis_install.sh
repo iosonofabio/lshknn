4b4b6912f4d4b481809e1433fa3d17276fe734af
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-14 18:16:21 -0800

    Explicit call to requirements.txt

--- .travis_install.sh
+++ .travis_install.sh
@@ -2,9 +2,10 @@
 if [ $TRAVIS_OS_NAME == 'osx' ]; then
   export PATH="$HOME/miniconda/bin:$PATH"
   source $HOME/miniconda/bin/activate
-  # Somehow we need this to execute the setup.py at all...
-  pip install numpy
 fi
+
+# Install requirements
+pip install -r requirements.txt
 
 # old setuptools also has a bug for extras, but it still compiles
 pip install -v '.[dataframes]'
