085ff8cb133493f0534b411c2b80f1b5a47ea592
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-15 16:57:24 -0800

    C++ code starts to make more sense

--- lshknn/lshknn.cpp
+++ lshknn/lshknn.cpp
@@ -6,6 +6,26 @@ namespace py = pybind11;
 
 using MatrixXdR = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
+// Class that stores pointers to the begin and end of a bit set.
+// Does not own the memory, and copies are shallow copies.
+// Implements low level operations on bit sets.
+class BitSetPointer {
+public:
+
+    // Begin and and pointers of the bit set.
+    uint64_t* begin;
+    uint64_t* end;
+
+    // Constructors.
+    BitSetPointer(uint64_t* begin=0, uint64_t* end=0) : begin(begin), end(end) {}
+    BitSetPointer(uint64_t* begin, uint64_t wordCount) : begin(begin), end(begin+wordCount) {}
+
+    // Return the number of 64-bit words in the bit set.
+    uint64_t wordCount() const { return end - begin; }
+
+}
+
+
 // Count the number of mismatching bits between two bit vectors.
 // Use SSE 4.2 builtin instruction popcount
 inline uint64_t countMismatches(
@@ -22,31 +42,22 @@ inline uint64_t countMismatches(
 
 // Compute the LSH similarity between two cells,
 // specified by their ids local to the cell set used by this Lsh object.
-double computeCellSimilarity(CellId localCellId0, CellId localCellId1)
+double computeCellSimilarity(
+    const BitSetPointer signature0,
+    const BitSetPointer signature1,
+    std::vector<double>& similarityTable)
 {
-    // Access the LSH signatures for the two cells.
-    const BitSetPointer signature0 = getSignature(localCellId0);
-    const BitSetPointer signature1 = getSignature(localCellId1);
-
     // Count the number of bits where the signatures of these two cells disagree.
     const size_t mismatchingBitCount = countMismatches(signature0, signature1);
 
     // Return the similarity corresponding to this number of mismatching bits.
     return similarityTable[mismatchingBitCount];
 }
-size_t computeMismatchCount(CellId localCellId0, CellId localCellId1)
-{
-    // Access the LSH signatures for the two cells.
-    const BitSetPointer signature0 = getSignature(localCellId0);
-    const BitSetPointer signature1 = getSignature(localCellId1);
-
-    // Count the number of bits where the signatures of these two cells disagree.
-    return countMismatches(signature0, signature1);
-}
-
 
 // Compute the similarity (cosine of the angle) corresponding to each number of mismatching bits.
-void computeSimilarityTable(size_t lshCount, std::vector<double>& similarityTable)
+void computeSimilarityTable(
+    const size_t lshCount,
+    std::vector<double>& similarityTable)
 {
     // Initialize the similarity table.
     similarityTable.resize(lshCount + 1);
@@ -72,32 +83,34 @@ void computeSimilarityTable(size_t lshCount, std::vector<double>& similarityTabl
 // Python Interface
 ///////////////////////////////////////////////////////////
 void knn_from_signature(
-		py::EigenDRef<Eigen::MatrixXd> signature,
-		py::EigenDRef<Eigen::MatrixXd> knn,
-		py::EigenDRef<Eigen::MatrixXd> similarity,
-		py::EigenDRef<Eigen::MatrixXd> n_neighbors,
-		int n,
-		int m,
-		int k,
-		double threshold) {
+    py::EigenDRef<Eigen::MatrixXd> signature,
+    py::EigenDRef<Eigen::MatrixXd> knn,
+    py::EigenDRef<Eigen::MatrixXd> similarity,
+    py::EigenDRef<Eigen::MatrixXd> n_neighbors,
+    const int n,
+    const int m,
+    const int k,
+    const double threshold) {
 
-	// signature is a vector containing 64 bit integers for all n cells, the number of integers for each cell is
-	// b = (1 + ((m - 1) div 64))
-	// so we can parse 0 to (b - 1) for cell 1, b to (2b - 1) for cell 2, etc.
-	int b = 1 + ((m - 1) / 64);
+    // signature is a vector containing 64 bit integers for all n cells, the number of 64bit for each cell is
+    // so we can parse 0 to (b - 1) for cell 1, b to (2b - 1) for cell 2, etc.
+    size_t wordCount = 1 + ((m - 1) / 64);
 
-	// Compute the similarity table with m bits
-	std::vector<double> similarityTable;
-	computeSimilarityTable((size_t)m, similarityTable);
-
-	//TODO: write the actual code!
-	for(int i=0; i < n; i++) {
-		if (i == 0)
-			knn(i, 0) = 4;
-		else
-			knn(i, 0) = 0;
-	}
-	// end TODO
+    // Compute the similarity table with m bits
+    std::vector<double> similarityTable;
+    computeSimilarityTable((size_t)m, similarityTable);
+    
+    //TODO: write the actual code!
+    // We gotta wrap chunks of signature as non-memory-owning BitSetPointer to keep some resemblance to Paolo's
+    // original API
+    //TODO
+    for(int i=0; i < n; i++) {
+        if (i == 0)
+    	    knn(i, 0) = 4;
+        else
+    	    knn(i, 0) = 0;
+    }
+    // end TODO
 	
 }
 
