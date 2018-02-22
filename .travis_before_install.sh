1869c5815044598d2136b086534c33ccb83e829c
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-21 23:15:11 -0800

    Test OSX

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 if [ $TRAVIS_OS_NAME == 'linux' ]; then
+
   echo "Installing deps for linux"
   sudo apt-get -qq update
   sudo apt-get install -y cmake pkg-config
   ./.travis_install_eigen3.sh
+
 elif [ $TRAVIS_OS_NAME == 'osx' ]; then
+
   echo "Installing deps for OSX"
   if [ $PYTHON_VERSION == "2.7" ]; then
     CONDA_VER='2'
@@ -21,7 +24,10 @@ elif [ $TRAVIS_OS_NAME == 'osx' ]; then
   # Use pip from conda
   conda install -y pip
   pip --version
-  echo "FIXME: eigen3 and pkg-config come here!"
+
+  conda install -y -c anaconda pkg-config
+  conda install -y -c conda-forge eigen
+
 else
   echo "OS not recognized: $TRAVIS_OS_NAME"
   exit 1
