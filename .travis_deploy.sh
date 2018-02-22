1241ece2a6eb677ca5826db49e2bfc455200c8f8
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-21 23:51:16 -0800

    Deploy script improvements

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+case $1 in
+ testpypi) 
+  export PYPI=https://test.pypi.org/legacy/
+  export PYPY_IDX=https://test.pypi.org/simple/
+  ;;
+ pypi) 
+  export PYPI=https://upload.pypi.org/legacy/
+  export PYPY_IDX=https://pypi.python.org/simple/
+  export TWINE_PASSWORD=$TWINE_PASSWORD_PYPI
+  ;;
+ *)
+  echo "Deploy script arg not understood: $1"
+  exit 1
+  ;;
+esac
+ 
 echo "Deploy onto: $PYPI"
 
 pip install twine
