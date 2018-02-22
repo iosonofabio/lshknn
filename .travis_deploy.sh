1b0a224d0a5969d439dfd070ef395a7e8ead368b
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-02-21 23:36:35 -0800

    Deploy script changes

--- .travis_deploy.sh
+++ .travis_deploy.sh
@@ -4,7 +4,8 @@ echo "Deploy onto: $PYPI"
 pip install twine
 python setup.py sdist
 distfile=$(ls dist/*.tar.gz)
-twine upload --repository testpypi --repository-url $PYPI --skip-existing
+twine upload --repository testpypi --repository-url $PYPI --skip-existing $distfile
 
 echo "Test install from pip"
-sudo pip install --index-url $PYPY_IDX your-package lshknn
+sudo pip uninstall lshknn
+sudo pip install --index-url $PYPY_IDX lshknn
